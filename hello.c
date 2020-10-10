7731c747df35094567e132e4e39ab4cc0953ea84
nicholas <duhnick@John.domain.com>
2020-10-10 12:49:55 -0400

    updated

--- hello.c
+++ hello.c
@@ -3,8 +3,9 @@
 int main(void)
 {
 	//This should get pulled by git pull
-
+	// edited line for the workshop branch
 	//update
+	printf("HEllo WORKSHOP branch");
 	printf("hello world!");
 	return 0;
 }
